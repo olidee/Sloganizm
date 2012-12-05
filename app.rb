0be736cb517e1a213a02e45f15edfefa99048ddf
oli <olidesborough@gmail.com>
2012-12-05 20:24:23 +0000

    added sanitize for html and css

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'sanitize'
 
 class App < Sinatra::Application
   get '/' do
@@ -9,7 +10,7 @@ class App < Sinatra::Application
 
   post '/' do
     @slogan = (params[:slogan].to_s.empty? ? "wake up! life is short" : params[:slogan])
-    @slogan = @slogan.upcase
+    @slogan = Sanitize.clean(@slogan)
     @title = @slogan
     haml :display, :locals => {:slogan => @slogan}
   end
